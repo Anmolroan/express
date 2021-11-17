2a5272a5ca9c7d0355630cb056432fa401dd92b0
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2021-11-18 00:31:29 +0530

    express day one assignment

---
+++ express own/expressjswork/expserver.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+app.get("/",(req,res)=>{
+res.send("welcome to  my channel")
+})
+app.get("/about",(req,res)=>{
+    res.send("welcome to  my channel about")
+    })
+    app.get("/contact",(req,res)=>{
+        res.send("welcome to  my channel contact")
+        })
+        app.get("/users",(req,res)=>{
+            res.sendFile(__dirname+"/db.json");
+            })
+app.listen(3000,()=>{
+    console.log("server is running on 3000")
+});
+
